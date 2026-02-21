f07cab9750ea342274a792deec0274f604120496
matias <matiasbrighent5@gmail.com>
2026-02-21 15:25:19 -0300

    configurando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCss = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
+import gulp from 'gulp';
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+import cleanCss from 'gulp-clean-css';
+
+const sass = gulpSass(dartSass);
 
 // Tarefa para compilar SCSS em CSS
-
 function styles(){
-    return gulp.src('src/scss/**/*.scss') // Seleciona todos os arquivos SCSS
-        .pipe(sass({outputStyle: 'compressed'}))
+    return gulp.src('src/styles/*.scss') // Seleciona todos os arquivos SCSS
+        .pipe(sass({
+            outputStyle: 'compressed',
+            sourceMap: false
+        }).on('error', sass.logError))
+        .pipe(cleanCss())
         .pipe(gulp.dest('./dist/css'));
 }
 
+// Tarefa para copiar imagens
+function images(){
+    return gulp.src('src/images/**/*', {nodir: true})// Seleciona todas as imagens
+        .pipe(gulp.dest('./dist/images'));
+}
+
 //exporta a tarefa para ser executada via linha de comando
+export default gulp.parallel(styles, images);
 
-exports.default = gulp.parallel(styles);
-
-exports.watch = function(){
-    gulp.watch('src/scss/**/*.scss', gulp.parallel(styles));
+export function watch(){
+    gulp.watch('src/styles/*.scss', gulp.parallel(styles));
 }
